5dc24e301a799bbe2b9d74e88043fa3f108a3146
Jesus Andres Sanchez Quintero <andresquintero2509>
2021-02-17 11:41:58 -0500

    Se añade clase de String

---
+++ Fundamentos/HolaMundoJava/src/CadenaCaracteres.java
@@ -0,0 +1,10 @@
+//Clase para la gestión de Strings
+public class CadenaCaracteres {
+    public static void main(String args[]){
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Adiós";
+        System.out.println(miVariableCadena);
+    } 
+}
