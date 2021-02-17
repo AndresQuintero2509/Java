db53edab95dcb02fa9889f69fe0927920aa6f965
Jesus Andres Sanchez Quintero <andresquintero2509>
2021-02-17 14:18:53 -0500

    Se añade cadena e inferencia de tipos

---
+++ Fundamentos/HolaMundoJava/src/InferenciaTipos.java
@@ -0,0 +1,10 @@
+// var - Inferencia de tipos en >= Java 10
+public class InferenciaTipos {
+    public static void main(String agrs[]){
+        var miVariableEntera2 = 10;
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        
+        var miVariableCadena = "Hola var";
+        System.out.println("miVariableCadena = " + miVariableCadena);
+    }
+}
