05af6015eaa092bfe4939b6cad2dd7cbb954ea08
Jesus Andres Sanchez Quintero <andresquintero2509>
2021-02-17 16:29:41 -0500

    Caracteres Especiales

---
+++ Fundamentos/HolaMundoVariables/src/CaracteresEspeciales.java
@@ -0,0 +1,13 @@
+
+public class CaracteresEspeciales {
+
+    public static void main(String args[]) {
+        var nombre = "Ana Maria";
+
+        System.out.println("Nueva linea: \n" + nombre); // Imprime en una nueva linea
+        System.out.println("Tabulador: \t" + nombre); // Imprime un tabulador
+        System.out.println("Retroceso: \b" + nombre); // Borra un espacio
+        System.out.println("Comilla Simple: '" + nombre + "'"); // Imprime comillas simples
+        System.out.println("Comilla doble: \"" + nombre + "\""); // Imprime comillas dobles
+    }
+}
